06a08f5e15987dc91fa60c9b13b68b73885b8d25
IsisAriele <isisariele1@gmail.com>
2024-11-30 22:56:21 -0300

    Cria crud de livros com express

--- src/app.js
+++ src/app.js
@@ -1,9 +1,53 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); 
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor dos aneis"
+    },
+    {
+        id: 2,
+        titulo: "O hobbit"
+    }
+]
+
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
 
 app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js")
+    res.status(200).send("Curso de Node.js"); // envio da resposta é uma string
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros); // envio da resposta em json
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros); // facilitando pra ver como esta tudo
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("O livro foi excluído");
 })
 
 export default app;
